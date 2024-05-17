ee820af96a753ff86a41621343df33d69ff8df6a
Jaewon <wefhio1985@gmail.com>
2024-05-17 12:13:55 +0900

    영화 데이터를 받기 위한 API

--- movies/models.py
+++ movies/models.py
@@ -4,15 +4,15 @@ from django.db import models
 class Movie(models.Model):
   movie_id = models.CharField(max_length=100, primary_key=True)
   title = models.TextField()
-  overview = models.TextField()
-  poster_path = models.TextField()
-  poster_path = models.TextField()
-  release_date = models.DateField()
-  runtime = models.IntegerField()
-  vote_average = models.FloatField()
-  genres = models.ManyToManyField('Genre', related_name=movies)
+  overview = models.TextField(null=True)
+  backdrop_path = models.TextField(null=True)
+  poster_path = models.TextField(null=True)
+  release_date = models.DateField(null=True)
+  vote_average = models.FloatField(null=True)
+  adult = models.BooleanField(default=False)
+  genres = models.ManyToManyField('Genre', related_name="movies")
 
 
 class Genre(models.Model):
+    genre_id = models.CharField(max_length=100, primary_key=True)
     name = models.CharField(max_length=50)
-    
