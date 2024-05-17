b2fed15f9f14d637329300a5b75268c651284a6a
Jaewon <wefhio1985@gmail.com>
2024-05-17 09:13:19 +0900

    feat-Movie, Genre 모델 작성

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+  movie_id = models.CharField(max_length=100, primary_key=True)
+  title = models.TextField()
+  overview = models.TextField()
+  poster_path = models.TextField()
+  poster_path = models.TextField()
+  release_date = models.DateField()
+  runtime = models.IntegerField()
+  vote_average = models.FloatField()
+  genres = models.ManyToManyField('Genre', related_name=movies)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+    
