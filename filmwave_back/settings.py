83652a21b1825622f315dfb3e928b1a6a203ed63
JaewonChoi <wefhio1985@gmail.com>
2024-05-18 17:33:37 +0900

    feat-그룹생성/그룹모델생성

--- filmwave_back/settings.py
+++ filmwave_back/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts',
     'movies',
-    'groups'
+    'groups',
     # django rest framework
     'rest_framework',
     'rest_framework.authtoken',
