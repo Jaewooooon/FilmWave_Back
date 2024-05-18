318b77be050d6882375463be4ff8bffa0680a287
JaewonChoi <wefhio1985@gmail.com>
2024-05-18 15:23:08 +0900

    feat-그룹생성/view.py 함수 생성

--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, get_list_or_404
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def group_list(request):
+  pass
