3769bbfc48e63479bc1c0d409f98c977262ee7b6
JaewonChoi <wefhio1985@gmail.com>
2024-05-18 18:59:42 +0900

    feat-그룹조회

--- groups/serializers.py
+++ groups/serializers.py
@@ -1,8 +1,35 @@
 from rest_framework import serializers
-from .models import Group
+from .models import Group, MemberShip
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username',)
+
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = '__all__'
+
+
+class GroupListSerializer(serializers.ModelSerializer):
+    admin = serializers.SerializerMethodField()
+    members_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Group
+        fields = '__all__'
+        
+    def get_admin(self, obj):
+        admin = MemberShip.objects.filter(group=obj, role='admin').select_related('user').first()
+        if admin:
+            return UserSerializer(admin.user).data
+        return None
+    
+    def get_members_count(self, obj):
+        return MemberShip.objects.filter(group=obj).count()
 
