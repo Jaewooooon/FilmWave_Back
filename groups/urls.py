6b3d0ff734b64d748ac620343a74e925d42fe7c3
JaewonChoi <wefhio1985@gmail.com>
2024-05-19 13:01:58 +0900

    feat-그룹탈퇴

--- groups/urls.py
+++ groups/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('<int:group_id>/', views.group_detail),
     path('<int:group_id>/membership-requests/', views.membership_request_list),
     path('<int:group_id>/membership-requests/<int:membership_request_id>/', views.membership_request_detail),
+    path('<int:group_id>/membership/', views.group_leave),
 ]
