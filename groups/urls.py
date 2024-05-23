1ac47e5cd27f43a04231e5ed27d2a74758e73adc
JaewonChoi <wefhio1985@gmail.com>
2024-05-23 20:14:07 +0900

    feat-사용자 그룹 목록

--- groups/urls.py
+++ groups/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.group_list),
-    path('<str:username>/', views.my_group_list),
-    path('<int:group_id>/', views.group_detail),
+    path('<str:username>/approved/', views.my_group_list),
     path('<int:group_id>/', views.group_detail),
     path('<int:group_id>/membership-requests/', views.membership_request_list),
     path('<int:group_id>/membership-requests/<int:membership_request_id>/', views.membership_request_detail),
